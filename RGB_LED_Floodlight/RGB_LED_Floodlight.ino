ee537a06c1f5eb0a1b546ec19e8397352d94ece7
Peter Deeming <pdeeming@varian.com>
2020-05-05 13:01:11 +0100

    Final working (without I2C)

--- RGB_LED_Floodlight/RGB_LED_Floodlight.ino
+++ RGB_LED_Floodlight/RGB_LED_Floodlight.ino
@@ -1,4 +1,9 @@
 /*
+ * v04 is a branch from v03 for a Floodlight hack
+ * Using D1 mini, and a commercially bought 12V LED Floodlight
+ *  - remove 200mA limit
+ *  - reverse polarity from LEDs since 0=ON
+ * 
  * v03 compile for 'D1 R2 and mimi', i am using D1 mini
  * pins were 4,5,16 on 8266 12E module, now 2,4,5
  * 
@@ -20,6 +25,11 @@ const int redPin = 2;
 const int greenPin = 4;
 const int bluePin = 5;
 
+int red = 0;            // initialise with LED off
+int green = 0;
+int blue = 0;
+  
+
 void setup() {
   Serial.begin(115200);
 
@@ -33,20 +43,20 @@ void loop() {
   while (Serial.available() > 0) {
 
     // look for valid integers in the incoming serial stream:
-    int red = Serial.parseInt();
-    int green = Serial.parseInt();
-    int blue = Serial.parseInt();
+    red = Serial.parseInt();
+    green = Serial.parseInt();
+    blue = Serial.parseInt();
 
     // look for the newline:
     if (Serial.read() == '\n') {
       // constrain the values to 0 - 255
-      red = 255 - constrain(red, 0, 255);
-      green = 255 - constrain(green, 0, 255);
-      blue = 255 - constrain(blue, 0, 255);
+      red = constrain(red, 0, 255);
+      green = constrain(green, 0, 255);
+      blue = constrain(blue, 0, 255);
 
       // map from 0-255 to 0-1023 since 8266 uses this as pwm range.
       // this is not required for Arduino which uses pwm range 0-255
-      red = map(red, 0, 255, 0, 1023);
+      red = map(red, 0, 255, 0, 1023);     // max range 1023.  choose less to limit I through LED
       green = map(green, 0, 255, 0, 1023);
       blue = map(blue, 0, 255, 0, 1023);
 
