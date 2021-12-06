9fa7758bc724da136adb42926b826ce5e91d32d1
pete201 <peterdeeming@hotmail.com>
2021-12-06 19:35:12 +0000

    try machine-gunning messages from supervisor

--- RGB_supervisor/RGB_supervisor.ino
+++ RGB_supervisor/RGB_supervisor.ino
@@ -7,13 +7,14 @@
 *       data in: Target,R,G,B
  *      data out: HopCount,Target,R,G,B* 
  *     
- *     waits for response from last in daisy chain
- *     if data is corrupted then send again.
+ * supervisor continuously sends the latest message from Serial (Python)
+ * we do this to solve the problem of messages getting lost because of our crummy mySerial data transmission
  * 
  * Use Serial for comms with PC and SoftwareSerial for comms with next device
  * Main reason for this is that i can't physically fit a USB connector into my floodlights!
  */
 
+#include <arduino>
 #include <SoftwareSerial.h>
 // pins for software serial interface
 const int serialRxPin = 4;  // white wire
@@ -33,6 +34,8 @@ void setup() {
   // Software serial is used for I/O
   mySerial.begin(57600);
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, 1);
 }
 
 void loop() {
@@ -43,14 +46,15 @@ void loop() {
     green = Serial.parseInt();
     blue = Serial.parseInt();
     // look for the newline:
-    if (Serial.read() == '\n'){
-      // send data to next target, adding hopCount = 1
-      mySerial.printf("%d,%d,%d,%d,%d\n", hopCount, target, red, green, blue);
+    if (Serial.read() != '\n'){
+      // blink the onboard LED to indicate data error
+      digitalWrite(LED_BUILTIN, 0);   // turn ON built in LED
+      delay(10);
+      digitalWrite(LED_BUILTIN, 1);   // turn OFF built in LED
     }
   }
 
-  while (mySerial.available() > 0) {
-    Serial.print(mySerial.read());
-  }
+  mySerial.printf("%d,%d,%d,%d,%d\n", hopCount, target, red, green, blue);
+  delay(10);    // this delay is to give LEDs a chance to read and process messages
 
 }
