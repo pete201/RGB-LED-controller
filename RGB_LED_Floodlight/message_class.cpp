de93cb6e3d32494c546f27809582d1cab9b85713
pete201 <peterdeeming@hotmail.com>
2022-01-08 18:59:50 +0000

    works well.  message class in separate files

---
+++ RGB_LED_Floodlight/message_class.cpp
@@ -0,0 +1,58 @@
+// message_class.h
+#include "message_class.h"
+#include <Arduino.h>
+
+
+message::message(int red, int green, int blue){
+  // constructor for message
+  redPin = red;
+  greenPin = green;
+  bluePin = blue;
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void message::buildMessage(int data) {
+  // on incoming serial, add next data item to array and increase counter (arrayIndex)
+  Serial.printf("buildMessage: adding data %d to array position %d\n", data, arrayIndex);
+  messageArray[arrayIndex] = data;
+  arrayIndex++;
+}
+
+void message::endMessage() {
+  // if we have correct number of data elements, set LEDs
+  if (arrayIndex == 5){
+    if (messageArray[target] == messageArray[hopCount] || messageArray[target] == 0){
+          // set LEDs on this device
+          writeRGB(messageArray[red],messageArray[green],messageArray[blue]);
+          Serial.printf("endMessage: %d,%d,%d,%d,%d\n", messageArray[hopCount], messageArray[target], messageArray[red], messageArray[green], messageArray[blue]);
+    } else {
+      Serial.println("endMessage: message not for this LED");
+    }
+  } else {
+    Serial.print("endMessage: incorrect buffer size: ");       // if wrong array size then do nothing
+    Serial.println(arrayIndex);
+  }
+  // regardless of correct number of elements, reset counter for next message
+  arrayIndex = 0;
+}
+
+void message::writeRGB(int r, int g, int b) {
+  // function for setting LEDs
+  // constrain the values to 0 - 255
+  r = constrain(r, 0, 255);
+  g = constrain(g, 0, 255);
+  b = constrain(b, 0, 255);
+
+  // map from 0-255 to 0-1023 since 8266 uses this as pwm range.
+  // this is not required for Arduino which uses pwm range 0-255
+  r = map(r, 0, 255, 0, 1023); // max range 1023.  choose less to limit I through LED
+  g = map(g, 0, 255, 0, 1023);
+  b = map(b, 0, 255, 0, 1023);
+
+  // fade the red, green, and blue legs of the LED:
+  analogWrite(redPin, r);
+  analogWrite(greenPin, g);
+  analogWrite(bluePin, b);
+}
---
+++ RGB_LED_Floodlight/message_class.h
@@ -0,0 +1,26 @@
+// message_class.h
+
+#ifndef MESSAGE_CLASS_H
+#define MESSAGE_CLASS_H
+
+class message {
+  public:
+    message (int, int, int);
+    void resetCounter() {arrayIndex = 0;}
+    void buildMessage(int);
+    void endMessage();
+    void writeRGB(int, int, int);
+  private:
+    int redPin;
+    int greenPin;
+    int bluePin;
+    const int hopCount = 0;           // counts the number of times message is relayed
+    const int target = 1;             // the device a serial message is addressed to
+    const int red = 2;                // led's
+    const int green = 3;
+    const int blue = 4;
+    int messageArray [16];            // array used to build message piece by piece
+    int arrayIndex = 0;
+};
+
+#endif
