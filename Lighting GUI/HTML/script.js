066bea70db1904bba261303f787b74e89e22ece8
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2022-07-27 12:51:15 +0100

    Added fade and more scenes. Looks nice aswell

--- Lighting GUI/HTML/script.js
+++ Lighting GUI/HTML/script.js
@@ -11,6 +11,7 @@ window.onload=function(){
             encoder = new TextEncoder();
             writer = port.writable.getWriter();
             // writer.write(encoder.encode("0,0,0,0\n"));
+            connectButton.remove();
             rgb(0,0,0,0);
             // writer.releaseLock();
         });
@@ -18,5 +19,21 @@ window.onload=function(){
 };
 
 function rgb(light,red,green,blue){
+    redVal = red;
+    greenVal = green;
+    blueVal = blue;
     writer.write(encoder.encode(`${light},${red},${green},${blue}\n`));
 }
+
+function fade(light,red,green,blue,fadeLength){
+    differenceRed = (redVal-red)/(fadeLength*10)
+    differenceGreen = (greenVal-green)/(fadeLength*10)
+    differenceBlue = (blueVal-blue)/(fadeLength*10)
+    interval = setInterval(function(){
+        redVal -= Math.round(differenceRed)
+        greenVal -= Math.round(differenceGreen)
+        blueVal -= Math.round(differenceBlue)
+        rgb(light,redVal,greenVal,blueVal);
+    },100);
+    setTimeout(function(){clearInterval(interval);rgb(light,red,green,blue)},fadeLength*1000);
+};
