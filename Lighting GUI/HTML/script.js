fea3fa32b51d1fdc7b460a60302136ae606fdf6b
PenguinBoy13 <archie.anthony@blueyonder.co.uk>
2022-08-01 13:47:36 +0100

    Added Colour Picker, console and make sure the fade does not go over 255 or below 0

--- Lighting GUI/HTML/script.js
+++ Lighting GUI/HTML/script.js
@@ -1,6 +1,7 @@
 window.onload=function(){
     if ("serial" in navigator) {
         connectButton = document.getElementById('connect');
+        dataText = document.getElementById('data');
         connectButton.addEventListener('click', async () => {
             // Prompt user to select any serial port.
             port = await navigator.serial.requestPort();
@@ -14,6 +15,9 @@ window.onload=function(){
             timeout = undefined;
             fadeTime = 1;
             delayTime = 1;
+            colourPicker = [255,255,255];
+            lightPicker = 0;
+            data = ["Connected"];
             rgb(0,0,0,0);
             // writer.releaseLock();
         });
@@ -25,6 +29,20 @@ function rgb(light,red,green,blue){
     greenVal = green;
     blueVal = blue;
     writer.write(encoder.encode(`${light},${red},${green},${blue}\n`));
+    // if(data.length>=3){
+    //     data.splice(0,1);
+    // };
+    data.push([light,red,green,blue])
+    if(data.length>500){
+        data.splice(0,1);
+    };
+    // dataText.innerHTML = data[0] + "\n" + data[1] + "\n" + data[2]
+    dataText.innerHTML = ""
+    for(let i=0;i<data.length;i++){
+        dataText.innerHTML += data[i] + "\n";
+    };
+    dataText.scrollTop=dataText.scrollHeight;
+    // dataText.innerHTML = data;
 }
 
 function fade(light,red,green,blue,fadeLength){
@@ -36,9 +54,27 @@ function fade(light,red,green,blue,fadeLength){
             redVal -= Math.round(differenceRed)
             greenVal -= Math.round(differenceGreen)
             blueVal -= Math.round(differenceBlue)
+            if(redVal<=0){
+                redVal = 0;
+            }
+            if(greenVal<=0){
+                greenVal = 0;
+            }
+            if(blueVal<=0){
+                blueVal = 0;
+            }
+            if(redVal>=255){
+                redVal = 255;
+            }
+            if(greenVal>=255){
+                greenVal = 255;
+            }
+            if(blueVal>=255){
+                blueVal = 255;
+            }
             rgb(light,redVal,greenVal,blueVal);
         },100);
-        setTimeout(function(){if(interval!=undefined){clearInterval(interval);rgb(light,red,green,blue)}},fadeLength*1000);
+        setTimeout(function(){if(interval!=undefined){clearInterval(interval);interval=undefined;;rgb(light,red,green,blue)}},fadeLength*1000);
     } else {
         differenceRed = (redVal-red)/(fadeTime*10)
         differenceGreen = (greenVal-green)/(fadeTime*10)
@@ -47,9 +83,27 @@ function fade(light,red,green,blue,fadeLength){
             redVal -= Math.round(differenceRed)
             greenVal -= Math.round(differenceGreen)
             blueVal -= Math.round(differenceBlue)
+            if(redVal<=0){
+                redVal = 0;
+            }
+            if(greenVal<=0){
+                greenVal = 0;
+            }
+            if(blueVal<=0){
+                blueVal = 0;
+            }
+            if(redVal>=255){
+                redVal = 255;
+            }
+            if(greenVal>=255){
+                greenVal = 255;
+            }
+            if(blueVal>=255){
+                blueVal = 255;
+            }
             rgb(light,redVal,greenVal,blueVal);
         },100);
-        setTimeout(function(){if(interval!=undefined){clearInterval(interval);rgb(light,red,green,blue)}},fadeTime*1000);
+        setTimeout(function(){if(interval!=undefined){clearInterval(interval);interval=undefined;rgb(light,red,green,blue)}},fadeTime*1000);
     }
 };
 
@@ -118,4 +172,15 @@ function chase(light,array,delay,fadeLength){
             };
         }, delayTime*1000)
     }
+};
+
+function hexToRGB(hex){
+    hex = hex.slice(1)
+    rgbhex = hex.match(/.{1,2}/g)
+    rgbValues = [
+        parseInt(rgbhex[0], 16),
+        parseInt(rgbhex[1], 16),
+        parseInt(rgbhex[2], 16)
+    ];
+    return rgbValues
 };
